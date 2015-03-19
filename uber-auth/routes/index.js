99bc639318202baf6e39edfa2d947ba512d11b2e
Mathew Bergt <mathew.bergt@gmail.com>
2015-03-18 23:32:06 -0800

    Implemented oauth2

--- uber-auth/routes/index.js
+++ uber-auth/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var uberStrategy = require('passport-uber');
+var OAuth2Strategy = require('passport-oauth2');
 var User = require('../models/user');
 
 /* GET home page. */
@@ -10,14 +10,18 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/auth/init', function (req, res, next) {
-	
-	passport.use(new uberStrategy({
-			clientID: req.body.client_id,
-			clientSecret: 'TrYjG6u07PKGKQt5Y4mM5qjPOee07fj4J9MBS3lb', // client secret kept server side only
-			callbackURL: "https://localhost:8080/auth/callback"
+
+	passport.use(new OAuth2Strategy({
+			authorizationURL: 'https://login.uber.com/oauth/authorize',
+	    tokenURL: 'https://login.uber.com/oauth/token',
+	    clientID: req.body.client_id,
+	    clientSecret: 'TrYjG6u07PKGKQt5Y4mM5qjPOee07fj4J9MBS3lb', // client secret kept server side only
+	    callbackURL: 'https://localhost:8080/auth/callback'
 		},
 		function(accessToken, refreshToken, profile, done) {
 			User.findOrCreate(profile, function (err, user) {
+				passport.session.accessToken = accessToken;
+				passport.session.refreshToken = refreshToken;
 				return done(err, user);
 			});
 		}
@@ -26,13 +30,13 @@ router.post('/auth/init', function (req, res, next) {
 	res.send({ result: 'success' });
 });
 
-router.get('/auth/doauth', passport.authenticate('uber'));
+router.get('/auth/doauth', passport.authenticate('oauth2'));
 
 router.get('/auth/callback', 
-  passport.authenticate('uber', { failureRedirect: '/?errMsg=Authentication Error' }),
+  passport.authenticate('oauth2', { failureRedirect: '/?errMsg=Authentication Error' }),
   function(req, res) {
     // Successful authentication, redirect home.
-    res.redirect('https://localhost:9000');
+    res.redirect('https://localhost:9000/#/access_token/' + passport.session.accessToken);
 });
 
 module.exports = router;
